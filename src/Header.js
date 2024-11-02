5bb3266f7e4caaf9bab51475d3baadab1fa6b1d3
root <root@kali>
2024-11-02 01:57:59 +0100

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+
+export default
+
+function Header(){
+   return(
+    <div style={{width:"100%",backgroundColor: 'lightblue',
+     padding: '20px',
+     textalign:' left',
+     fontSize:"30px",
+     color:'black',
+     fontFamily:'cursive'}}>
+        <p> Welcome to Rubber store.<br/> for cyber security . </p>
+        
+        
+    </div>
+);
+}
+
